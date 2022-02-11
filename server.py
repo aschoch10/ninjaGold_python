a9be1907959599cf3c2713d40c31e9474caf3bc9
Andrew <aschoch10@gmail.com>
2022-02-10 23:33:24 -0800

    project shell

---
+++ server.py
@@ -0,0 +1,18 @@
+import random
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = 'super, super secret key name'
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
